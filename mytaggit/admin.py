b51ee784cf5ac9ed23ae182e8f416aa6afa8daa9
hdknr <gmail@hdknr.com>
2017-09-26 07:30:05 +0000

    admin

--- mytaggit/admin.py
+++ mytaggit/admin.py
@@ -1,1 +1,17 @@
 # coding: utf-8
+
+from django.contrib import admin
+from .models import Tag, TaggedItem
+
+
+class TaggedItemInline(admin.StackedInline):
+    model = TaggedItem
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    inlines = [TaggedItemInline]
+    list_display = ["name", "slug"]
+    ordering = ["name", "slug"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ["name"]}
