026ea897c4d6ef420af0b83b5769dcc62fd26385
hdknr <gmail@hdknr.com>
2018-01-03 18:20:21 +0900

    keyword search(slug/name contains the value)

--- mytaggit/filters.py
+++ mytaggit/filters.py
@@ -1,12 +1,14 @@
-# coding: utf-8
-import rest_framework_filters as filters
+from django.utils.translation import ugettext_lazy as _
 from django.db.models import Q
+import rest_framework_filters as filters
 from . import models
 
 
 class TagFilter(filters.FilterSet):
-
-    is_active = filters.BooleanFilter(method='filter_is_actvie')
+    keyword = filters.CharFilter(
+        label=_('Keyword'), name='keyword', method='filter_keyword')
+    is_active = filters.BooleanFilter(
+        label=_('Is Active'), name='is_active', method='filter_is_actvie')
 
     class Meta:
         model = models.Tag
@@ -21,6 +23,10 @@ class TagFilter(filters.FilterSet):
             return queryset.filter(q)
         return queryset
 
+    def filter_keyword(self, queryset, name, value):
+        q = Q(slug='') | Q(name='') | Q(mytaggit_taggeditem_items__isnull=True)
+        return value and queryset.filter(
+            Q(name__icontains=value) | Q(slug__icontains=value)) or queryset
 
 
 class TaggedItemFilter(filters.FilterSet):
