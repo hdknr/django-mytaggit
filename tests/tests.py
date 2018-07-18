aa56a3125b347af35829c95d54870c9a61be4a78
hdknr <gmail@hdknr.com>
2018-07-18 14:59:31 +0900

    TaggedItem with Users test

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 from unittest import TestCase as UnitTestCase
 from .models import Food
 from mytaggit.models import Tag
@@ -20,3 +19,22 @@ class FoodTestCase(UnitTestCase):
         )
         tags = list(food.tags.all())
         self.assertTrue(tags[1].slug.startswith(tags[0].slug))
+
+    def test_user(self):
+        from django.contrib.auth.models import User
+        users = [
+            User.objects.create(
+                username=f'test{i}', email=f"test{1}@localhost")
+            for i in range(3)]
+        tags = ['野菜', '美味しい']
+        food = Food.objects.create()
+        food.tags.add_users(tags, users=[users])
+        #
+        self.assertTrue(
+            Tag.objects.filter(
+                name='野菜', 
+                mytaggit_taggeditem_items__users__username='test1').exists())
+        self.assertFalse(
+            Tag.objects.filter(
+                name='野菜', 
+                mytaggit_taggeditem_items__users__username='test3').exists())
