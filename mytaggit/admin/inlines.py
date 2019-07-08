dbbe5b98a2f92dd36dd88f9ca8cbb0e5da989c8f
hdknr <gmail@hdknr.com>
2019-07-08 13:00:37 +0900

    admin: item_link, tag_link

--- mytaggit/admin/inlines.py
+++ mytaggit/admin/inlines.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.urls import reverse
+from django.utils.html import mark_safe
 from ..models import TaggedItem
 from . import forms
 
 
 class TaggedItemInline(admin.TabularInline):
+    readonly_fields = ['item_link']
     model = TaggedItem
     raw_id_fields = ['users', 'content_type']
     extra = 1
+
+    def item_link(self, obj):
+        url = reverse(f"admin:{obj.content_type.app_label}_{obj.content_type.model}_change", args=[obj.object_id])
+        return mark_safe(f'<a href="{url}">{obj}</a>')
 
 
 class GenericTaggedItemInline(GenericTabularInline):
@@ -16,3 +23,10 @@ class GenericTaggedItemInline(GenericTabularInline):
     exclude = ['tag', 'value']
     extra = 1
     form = forms.TaggedItemForm
+    readonly_fields = ['tag_link']
+
+    def tag_link(self, obj):
+        if not obj.tag:
+            return ''
+        url = reverse(f"admin:{obj.tag._meta.app_label}_{obj.tag._meta.model_name}_change", args=[obj.tag.id])
+        return mark_safe(f'<a href="{url}">{obj.tag}</a>')
